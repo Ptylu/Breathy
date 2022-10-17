4d5a2df832b0a00115da549f93eef2857368bfc6
Ptuly <lucas.heiligenstein@gmail.com>
2022-10-17 23:39:54 +0800

    Default en cookie

--- assets/dist/js/cookie_lang.js
+++ assets/dist/js/cookie_lang.js
@@ -1,4 +1,11 @@
 
+// Check if cookie not initialised, we put en as defaut cookie (when first connection on website)
+if (typeof $.cookie("lang") === 'undefined'){
+    //myVariable is undefined
+    console.log("Cookie Undefined");
+    document.cookie = "lang="+"en";
+    location.reload();
+}
   // set html attribut with language
   document.documentElement.setAttribute("lang", $.cookie("lang"));
   // Set language
@@ -7,5 +14,4 @@
     document.cookie = "lang="+languageCode;
     //reload to take into account the new langage in the cookie
     location.reload();
-
   };
